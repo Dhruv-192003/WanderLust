dd3dfad6ad98b9819374318dc50bb4138f664403
Dhrub Ghosh <dhrubghosh2003@gmail.com>
2025-11-16 18:52:01 +0530

    added project files

---
+++ routes/review.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const router=express.Router({mergeParams: true});//merge to get the :id from app.js
+const wrapAsync =require("../utils/wrapAsync.js");
+const {validateReview, isLoggedIn, isReviewAuthor }=require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
+
+
+//Reviews post route
+router.post("/",isLoggedIn,validateReview, wrapAsync(reviewController.createReview));
+
+//Reviews delete route
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
+
+module.exports=router;
